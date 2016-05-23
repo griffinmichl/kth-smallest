9c3321a7e8e210d95da75197f13d771537aca941
Griffin Michl <griffinmichl@gmail.com>
2016-05-23 14:11:07 -0700

    implement swap, partition, and kthLargest

---
+++ index.js
@@ -0,0 +1,53 @@
+function swap(array, index1, index2) {
+  const tmp = array[index1]
+  array[index1] = array[index2]
+  array[index2] = tmp
+}
+
+function partition(nums, pivotIndex, left = 0, right = nums.length - 1) {
+  const pivotValue = nums[pivotIndex]
+  swap(nums, pivotIndex, right)  
+  let swapIndex = left
+  for (let i = left; i < right; i++) {
+    if (nums[i] < pivotValue) {
+      swap(nums, i, swapIndex)
+      swapIndex++
+    }
+  }
+  swap(nums, swapIndex, right)
+  return swapIndex
+}
+
+function kthLargest(nums, k) {
+  function kthLargestHelper(nums, k, left, right){
+    const pivotIndex = Math.floor(Math.random() * (right - left) + left)
+    const resultIndex = partition(nums, pivotIndex, left, right)
+    if (resultIndex === k) {
+      return nums[resultIndex]
+    } else if (resultIndex < k) {
+      return kthLargestHelper(nums, k, resultIndex+1, right)
+    } else {
+      return kthLargestHelper(nums, k, left, resultIndex - 1)
+    }
+  }
+
+  if(!Array.isArray(nums)) {
+    throw new TypeError(`nums should be array, got ${typeof nums}`)
+  }
+
+  if (!Number.isInteger(k)) {
+    throw new TypeError(`k should be integer, got ${typeof k} `)
+  }
+
+  if (k < 0 || k >= nums.length) {
+    return undefined
+  }
+
+  return kthLargestHelper(nums, k, 0, nums.length - 1)
+}
+
+module.exports = {
+  kthLargest,
+  swap,
+  partition,
+}
